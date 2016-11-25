256ff79e5961c8ab2d91c19285f0993907867fad
Quentin Smith <shengjitian@gmail.com>
2016-11-25 22:37:59 +0800

    sidebar active status front done

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,20 @@
 $(function () {
+	//	sidebar
+	(function activeToggle() {
+		var li = $('.sidebar .nav li');
+		$.each(li, function(index, element){
+			//console.log('index: ' + index);
+			//console.log('value: ' + $(element).find('a').attr('class'));
+			$(element).click(function() {
+				rmActive(li);
+				$(this).attr('class', 'active');
+			});
+		});
+	})();
 
+	function rmActive(ele) {
+		$.each(ele, function(index, value) {
+			$(value).removeClass();
+		});
+	}
 });
